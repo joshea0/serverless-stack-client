18dddd6500f0c2e8bc7d5fde9ab638a302f5a1cf
Joseph O'Shea <joshea0@gmail.com>
2021-01-13 09:52:41 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "joshea-serverless-stack-notes",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://oi3p58sts9.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_CrqQLKhYM",
+    APP_CLIENT_ID: "5qn1f1pg17pao59bq2sjei1i8r",
+    IDENTITY_POOL_ID: "us-west-2:e7552a1f-bb6d-4a3d-aa1b-56eae342098f",
+  },
+};
+
+export default config;
